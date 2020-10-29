a7b086dc49f41a7eabc54b3e3e5f70377560b0aa
mmcgaw182 <40709462+mmcgaw182@users.noreply.github.com>
2020-10-29 02:49:24 -0400

    Add files via upload

--- StreamlitApp/src/pages/data.py
+++ StreamlitApp/src/pages/data.py
@@ -6,6 +6,8 @@ import json
 import seaborn as sns
 from IPython.core.display import display, HTML
 sns.set()
+from PIL import Image
+
 
 def import_speeches():
         speech = pd.read_csv('data/speeches.csv')
@@ -23,17 +25,62 @@ def write():
     # US Presidential Election Campaign Speeches
 
     ## The Data
+    
+    In our initial assessment of data sources, we realized that there were not 
+    any data sources that had aggregated campaign speeches into a single 
+    repository. Some sights had posted transcripts of extremely high profile 
+    speeches, with supplementary analysis, but nobody was posting transcripts 
+    for the smaller, daily stops of the campaign trail. We needed access to 
+    more data in order to conduct a thorough exploration of the data.
+    
+    To get around this, we decided to create a scraper that would capture 
+    speeches, interviews, and debates using YouTube's API. In one API call, we 
+    would search for speeches made by any candidates on that specific day that 
+    were over 20 minutes (in order to avoid search results such as clips or 
+    news stories). In the second API call, we would then write the automated 
+    video captions and any associated metadata into a JSON file. By having all 
+    of our speeches coming from the same site, we had the advantage of 
+    everything being formatted in a similar manner.
+    
+    As these are video captions and not true transcripts, the structure is a 
+    bit unusual. Each caption appears as its own line (as it would in the 
+    video), with an associated start time and duration, representing when that 
+    caption appeared in the video. See examples below.
 
     """)
 
+    image1 = Image.open('StreamlitApp/json_sample.png')
+    image2 = Image.open('StreamlitApp/caption.png')
+
+    st.image(image1, caption='Sample of captions format')
+    st.image(image2, caption='Captions as they appear in videos',
+             use_column_width=True)
+    
+    
+    st.write("""
+    ### Metadata
+    
+    The JSON file includes a variety of metadata, including:
+        
+    - title
+    - channel
+    - description
+    - publishedAt (date)
+    - tags
+    - commnetcount
+    - likecount
+    - dislikecount
+    - viewcount
+
+    """)
 
     st.subheader('Speech library')
     st.write(speech[["id","speaker","date","title","transcript_type"]])
 
-    c_picker = st.selectbox('Candidate:', speech['speaker'].unique(), index=0)
+#    c_picker = st.selectbox('Candidate:', speech['speaker'].unique(), index=0)
 
-    bow2 = speech.copy()
-    bow2['day'] = bow2['date'].str[:10]
-    dates = bow2[bow2['speaker'] == c_picker].groupby(['day']).agg({'speech': 'count'}).reset_index()
+#    bow2 = speech.copy()
+#    bow2['day'] = bow2['date'].str[:10]
+#    dates = bow2[bow2['speaker'] == c_picker].groupby(['day']).agg({'speech': 'count'}).reset_index()
 
-    st.bar_chart(dates)
+#    st.bar_chart(dates)
--- StreamlitApp/src/pages/summary.py
+++ StreamlitApp/src/pages/summary.py
@@ -6,31 +6,69 @@ import json
 import seaborn as sns
 from IPython.core.display import display, HTML
 sns.set()
+import altair as alt
 
 def import_speeches():
         speech = pd.read_csv('data/speeches.csv')
         return speech
+    
+def import_topics():
+        topics = pd.read_csv('data/topic_modeling.csv')
+        return topics
 
 #data_load_state = st.text('Loading data...')
 #speeches = load_speeches()
 #bow = load_bow()
 speech = import_speeches()
+topics = import_topics()
 #data_load_state.text('Loading data...done!')
 
 def write():
     
     st.write("""
     # US Presidential Election Campaign Speeches
+    
+    ## Topic Modeling
+    
+    Using Latent Dirichlet Allocation (LDA), we are able to model the topics
+    that candidates discuss by showing the most representative words for each 
+    topic, and show what proportion of their time each candidate dedicated to 
+    certain topics.
     """)
 
+    tops = alt.Chart(topics).transform_fold(['trump', 'biden', 'pence', 'harris'],
+      as_=['speaker', 'value']).mark_bar().encode(
+      x='value:Q',
+      y=alt.Y('topterms:N', sort='-x'),
+      color='speaker:N',
+      order=alt.Order(
+      'speaker:N',
+      sort='ascending')
+    )
+          
+    st.altair_chart(tops, use_container_width=True)
+        
+    st.write("""
+    We can see that Biden and Harris bring a very consistent message, with 
+    broad messaging focused on the nation as a whole, with health care and jobs
+    as the top priorities. They also bring more attention to focused topics 
+    such as the COVID-19 pandemic. In contrast, Trump focuses on what he sees 
+    as his strengths, with major topics surrounding jobs, dollars, and China. 
+    Pence is mainly delegated to one topic that seems to be purely about Trump
+    vs. Biden, which makes sense since the majority of his speeches are his 
+    introducing Trump at rallies, setting the stage for the stakes of the election.
+    
+    """)
+    
+    
 
     st.subheader('Speech library')
     st.write(speech[["id","speaker","date","title","transcript_type"]])
 
-    c_picker = st.selectbox('Candidate:', speech['speaker'].unique(), index=0)
+#    c_picker = st.selectbox('Candidate:', speech['speaker'].unique(), index=0)
 
-    bow2 = speech.copy()
-    bow2['day'] = bow2['date'].str[:10]
-    dates = bow2[bow2['speaker'] == c_picker].groupby(['day']).agg({'speech': 'count'}).reset_index()
+#    bow2 = speech.copy()
+#    bow2['day'] = bow2['date'].str[:10]
+#    dates = bow2[bow2['speaker'] == c_picker].groupby(['day']).agg({'speech': 'count'}).reset_index()
 
-    st.bar_chart(dates)
+#    st.bar_chart(dates)
